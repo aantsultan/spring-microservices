a81bf9d2c111d72ed3ce2132512a8cd0db820250
Aant <aant.sultan@gmail.com>
2023-03-30 14:37:45 +0700

    adding module exchange-conversion-service

---
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceMain.java
@@ -0,0 +1,12 @@
+package com.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceMain {
+
+    public static void main (String[] args){
+        SpringApplication.run(CurrencyConversionServiceMain.class, args);
+    }
+}
