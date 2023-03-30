e006ba2c7cf0b6660fd019bfa5d3e6a0a91690f4
Aant <aant.sultan@gmail.com>
2023-03-30 15:52:10 +0700

    remove url in currency exchange proxy for testing load balancer

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange", url = "localhost:8000") // name of spring application name
+@FeignClient(name="currency-exchange") // name of spring application name
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
