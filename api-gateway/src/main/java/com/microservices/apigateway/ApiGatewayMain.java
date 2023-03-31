c941ee4cde5b0e136a683ffc9dcd2659737fd455
Aant <aant.sultan@gmail.com>
2023-03-31 13:26:33 +0700

    adding api-gateway

---
+++ api-gateway/src/main/java/com/microservices/apigateway/ApiGatewayMain.java
@@ -0,0 +1,12 @@
+package com.microservices.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayMain {
+
+    public static void main (String[] args){
+        SpringApplication.run(ApiGatewayMain.class, args);
+    }
+}
