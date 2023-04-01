a2ad995cf6f4ae1ea346a160043a573357bb60b3
Your <aant.sultan@gmail.com>
2023-04-01 13:44:16 +0700

    adding annotation ratelimiter and bulkhead for circuitbreaker advanced

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +18,16 @@ public class CircuitBreakerController {
     // method for retry api if there is error
     @GetMapping("/sample-api")
 //    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @RateLimiter(name="default") // 10s => 10000 calls to the sample api
+    @Bulkhead(name = "sample-api") //set max concurrent
     public String sampleApi(){
         logger.info("Sample Api call received");
-        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-                    String.class
-                );
-        return response.getBody();
+//        ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+//                    String.class
+//                );
+//        return response.getBody();
+        return "sample-api";
     }
 
     public String hardcodedResponse(Exception ex){
