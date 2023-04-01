3e968872fdb0d0703017fcc039b73034df4c1a6d
Your <aant.sultan@gmail.com>
2023-04-01 14:12:56 +0700

    adding zipkins (distribute tracing server) into microservices currency-exchange

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private  CurrencyExchangeRepository repository;
@@ -22,6 +26,7 @@ public class CurrencyExchangeController {
             @PathVariable String from,
             @PathVariable String to
     ){
+        logger.info("retrieveExchangeValue called with {} and {}", from, to);
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         if(currencyExchange == null){
