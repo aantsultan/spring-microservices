7a8d7bbdd4e90224102c241db243019153128d92
Your <aant.sultan@gmail.com>
2023-04-01 11:59:26 +0700

    adding annotation circuit breaker

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,6 +1,6 @@
 package com.microservices.currencyexchangeservice;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +15,8 @@ public class CircuitBreakerController {
 
     // method for retry api if there is error
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
     public String sampleApi(){
         logger.info("Sample Api call received");
         ResponseEntity<String> response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
